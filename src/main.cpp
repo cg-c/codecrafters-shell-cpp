1b16053636dfbaf0b866454185a20d417b7360b4
cg-c <52759655+cg-c@users.noreply.github.com>
2025-05-20 13:01:00 -0400

    [echo]

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ int main() {
     std::string input;
     std::getline(std::cin, input);
     if (input == "exit 0") return 0;
-    else if (input.substr(0, 4) == "echo") std::cout << input.substr(4) << std::endl;
+    else if (input.substr(0, 4) == "echo") std::cout << input.substr(5) << std::endl;
     else std::cout << input << ": command not found" << std::endl;
   }
 }
