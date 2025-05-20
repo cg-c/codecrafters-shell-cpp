415290bae686a1e124e8b565f5d61901900fd10d
cg-c <52759655+cg-c@users.noreply.github.com>
2025-05-20 13:28:43 -0400

    [type]

--- src/main.cpp
+++ src/main.cpp
@@ -15,9 +15,16 @@ int main() {
     std::cout << "$ ";
     std::string input;
     std::getline(std::cin, input);
-    std::string command = input.substr(5);
 
-    if (std::find(valid.begin(), valid.end(), command) != valid.end()) std::cout << command << " is a shell builtin" << std::endl;
-    else std::cout << command << ": not found" << std::endl;
+    if (input.find("type") != std::String::npos) {
+      std::string command = input.substr(5);
+      if (std::find(valid.begin(), valid.end(), command) != valid.end()) std::cout << command << " is a shell builtin" << std::endl;
+      else std::cout << command << ": not found" << std::endl;
+    }
+    else {
+      if (input == "exit 0") return 0;
+      else if (input.substr(0, 4) == "echo") std::cout << input.substr(5) << std::endl;
+      else std::cout << input << ": command not found" << std::endl;
+    }
   }
 }
