ed50344b5494a2a9bbe9ca5c1d0d04cd82c685b3
cg-c <52759655+cg-c@users.noreply.github.com>
2025-05-22 09:49:04 -0400

    [execute]

--- src/main.cpp
+++ src/main.cpp
@@ -97,7 +97,7 @@ int main() {
       if (redirect != std::string::npos) {
         std::string output = input.substr(5, redirect - 5);
         std::string file = tokens[tokens.size() - 1];
-        freopen(output.c_str(), "w", file.c_str());
+        freopen(output.c_str(), "w", std::filesystem::directory_entry entry(file););
       }
       else if (tokens[1].at(0) == '\'' && input[input.size() - 1] == '\''){
         std::string print = Quotes(input.substr(5), '\'');
