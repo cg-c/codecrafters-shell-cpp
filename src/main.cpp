ab15904cbb46a73e8f917fc999a67715dfc40093
cg-c <52759655+cg-c@users.noreply.github.com>
2025-05-22 08:54:16 -0400

    [execute]

--- src/main.cpp
+++ src/main.cpp
@@ -132,12 +132,12 @@ int main() {
       // quotes executable --> execute smt
       if (input[0] == '\'') {
         size_t last = input.find_last_of('\'');
-        std::string file = input.substr(last + 1);
+        std::string file = input.substr(last + 2);
         if (GetPath(std::getenv("PATH"), file) != "") system(file.c_str());
       }
       else if (input[0] == '\"') {
         size_t last = input.find_last_of('\"');
-        std::string file = input.substr(last + 1);
+        std::string file = input.substr(last + 2);
         if (GetPath(std::getenv("PATH"), file) != "") system(file.c_str());
       }
       else std::cout << input << ": command not found" << std::endl;
