7778a1453728bea15d2c26eda7fdff09e5fe12dc
cg-c <52759655+cg-c@users.noreply.github.com>
2025-05-22 10:03:01 -0400

    [execute]

--- src/main.cpp
+++ src/main.cpp
@@ -100,7 +100,7 @@ int main() {
         // std::string file = tokens[tokens.size() - 1];
         // std::ofstream outputFile(file);
         // freopen(output.c_str(), "w", outputFile);
-        std::string cmd = input.substr(5, redirect - 5) + input.substr(redirect + 1);
+        std::string cmd = tokens[tokens.size()] + " > " + input.substr(5, redirect - 6);
         system(cmd.c_str());
       }
       else if (tokens[1].at(0) == '\'' && input[input.size() - 1] == '\''){
