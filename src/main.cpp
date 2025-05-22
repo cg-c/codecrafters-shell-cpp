f59f0e3a16185c18cf5ae8f7e18529c2196945b0
cg-c <52759655+cg-c@users.noreply.github.com>
2025-05-22 10:11:23 -0400

    [execute]

--- src/main.cpp
+++ src/main.cpp
@@ -100,8 +100,7 @@ int main() {
         // std::string file = tokens[tokens.size() - 1];
         // std::ofstream outputFile(file);
         // freopen(output.c_str(), "w", outputFile);
-        std::string cmd = input.substr(redirect + 3) + " > " + input.substr(5, redirect - 6);
-        std::cout << cmd << std::endl;
+        std::string cmd = "ls" + input.substr(5, redirect - 5) + input.substr(redirect + 1);
         system(cmd.c_str());
       }
       else if (tokens[1].at(0) == '\'' && input[input.size() - 1] == '\''){
