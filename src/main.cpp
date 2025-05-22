7d2863630555893de9875a1c6886e0e9d6b2add9
cg-c <52759655+cg-c@users.noreply.github.com>
2025-05-22 09:55:49 -0400

    [execute]

--- src/main.cpp
+++ src/main.cpp
@@ -100,7 +100,8 @@ int main() {
         // std::string file = tokens[tokens.size() - 1];
         // std::ofstream outputFile(file);
         // freopen(output.c_str(), "w", outputFile);
-        std::string input = input.substr(5);
+        std::string input = input.substr(5, redirect - 6) + input.substr(redirect + 1);
+        std::cout << input << std::endl;
         system(input.c_str());
       }
       else if (tokens[1].at(0) == '\'' && input[input.size() - 1] == '\''){
