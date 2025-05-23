ff000021bc1623dc944a183e1af30e1c40c02f87
cg-c <52759655+cg-c@users.noreply.github.com>
2025-05-23 13:52:12 -0400

    [redirct]

--- src/main.cpp
+++ src/main.cpp
@@ -108,8 +108,8 @@ int main() {
         file = tokens[tokens.size() - 1];
         std::ofstream f;
         f.open(file);
-        if (!f.is_open()) f << output << std::endl;
-        else f.close();
+        f << output << std::endl;
+        f.close();
         std::cout << output << std::endl;
       }
       else if (appendStdout != std::string::npos) {
